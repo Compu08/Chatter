546a75c10da373d7246f685ec64b535c2b647539
Compu08 <gastonhernandez1995@gmail.com>
2022-05-31 13:25:26 -0300

    Commit Four

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,7 +2,7 @@ import { BiSearchAlt } from 'react-icons/bi';
 import { request, gql } from "graphql-request";
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { SearchBarProps } from '../utils/types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NotificationFailure } from './notifications';
 import { FILTER_QUERY } from '../utils/gql-querys';
 import { endpoint } from '../utils/gql-endpoint';
@@ -10,6 +10,11 @@ import { endpoint } from '../utils/gql-endpoint';
 function SearchBar({ userId, chatId }: SearchBarProps) {
 
     const [filter, setFilter] = useState<string>("");
+
+    const result = useQuery(["filterMsg", filter], () => {
+        if(filter != "")
+            return request(endpoint, FILTER_QUERY, { userId, chatId, filter })
+    })
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (chatId == "") {
@@ -21,10 +26,6 @@ function SearchBar({ userId, chatId }: SearchBarProps) {
         }
     }
 
-    const result = useQuery(["filterMsg", filter], () => {
-        return request(endpoint, FILTER_QUERY, { userId, chatId, filter })
-    })
-
     return (
         <div id="searchBar" className="d-flex flex-column px-4 mt-3 pb-3">
             <div className="searchBar w-100 py-1 px-3 d-flex flex-row gap-2 align-items-center">
@@ -34,10 +35,10 @@ function SearchBar({ userId, chatId }: SearchBarProps) {
 
             <div className="search-result">
                 <div className={filter != "" ? "results text-chatter-black scale1" : "results text-chatter-black"}>
-                    {result.data?.filterMessages.map( (message:any,i:any) => 
-                    <div className="result" key={i}>{
-                        message.message.length > 1 ? message.message : message.message.length
-                    }</div>
+                    {result?.data?.filterMessages.map((message: any, i: any) =>
+                        <div className="result" key={i}>{
+                            message.message.length > 1 ? message.message : message.message.length
+                        }</div>
                     )}
                 </div>
             </div>
